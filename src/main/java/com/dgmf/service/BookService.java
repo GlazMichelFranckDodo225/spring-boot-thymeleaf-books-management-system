0b43abb4c6dbae9bc9a5743148424cd6fd3b9009
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-08-14 01:46:30 +0200

    Implementation of the methods of the "Service" layer

--- src/main/java/com/dgmf/service/BookService.java
+++ src/main/java/com/dgmf/service/BookService.java
@@ -3,6 +3,12 @@ package com.dgmf.service;
 import com.dgmf.entity.Book;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
+    List<Book> findAllBooks();
+    Book findBookById(Long id);
+    Book addBook(Book book);
+    Book updateBook(Book book);
+    void deleteBookById(Long id);
 }
--- src/main/java/com/dgmf/service/impl/BookServiceImpl.java
+++ src/main/java/com/dgmf/service/impl/BookServiceImpl.java
@@ -1,12 +1,65 @@
 package com.dgmf.service.impl;
 
+import com.dgmf.entity.Book;
 import com.dgmf.repository.BookRepository;
 import com.dgmf.service.BookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
+
+    @Override
+    public List<Book> findAllBooks() {
+        List<Book> allBooks = (List<Book>) bookRepository.findAll();
+
+        return allBooks;
+    }
+
+    @Override
+    public Book findBookById(Long id) {
+        Optional<Book> foundBook = bookRepository.findById(id);
+
+        if(foundBook.isPresent()) {
+            return foundBook.get();
+        }
+
+        return null;
+    }
+
+    @Override
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public Book updateBook(Book book) {
+        Optional<Book> foundBook = bookRepository.findById(book.getId());
+
+        if(foundBook.isPresent()) {
+            Book retrievalBook = foundBook.get();
+            retrievalBook.setName(book.getName());
+            retrievalBook.setAuthor(book.getAuthor());
+            retrievalBook.setNumberOfPages(book.getNumberOfPages());
+            retrievalBook.setPublication(book.getPublication());
+
+            return bookRepository.save(retrievalBook);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteBookById(Long id) {
+        Book book = findBookById(id);
+
+        if(book != null) {
+            bookRepository.delete(book);
+        }
+    }
 }
